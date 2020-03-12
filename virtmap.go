38cf1098c5096a42daa733eec642e14f967353a3
Derek Crovo <dcrovo@arubanetworks.com>
2020-03-12 11:48:53 -0700

    Moved away from using channels and back to a simple mutex for synchronization / thread safety

--- virtmap.go
+++ virtmap.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"sort"
 	"strconv"
 	"strings"
+	"sync"
 )
-
-// errNodeNotFound is returned when the requested host is not present in the vmap
-var errNodeNotFound = errors.New("Node not found")
 
 // VHost is a virtual host which contains several virtual guests
 // State may be "up" or "down"
@@ -20,14 +17,15 @@ type VHost struct {
 }
 
 // VGuest is a virtual guest. Includes the name of its virtual host
-// State may be "running", "paused", or "shut" (i.e. shut down) as reported by "virsh list"
+// State may be "running", "paused", or "shut" (i.e. shut down)
+// as reported by "virsh list --all"
 type VGuest struct {
 	State string `json:"state"`
 	Host  string `json:"host"`
 }
 
-// Vmap is the main virtual map.  It contains a map of guests and
-// a map of hosts to support queries in either direction.
+// Vmap is the main virtual map type.  It contains a map of guests
+// and a map of hosts to support queries in either direction.
 type Vmap struct {
 	Hosts  map[string]VHost  `json:"hosts"`
 	Guests map[string]VGuest `json:"guests"`
@@ -38,13 +36,98 @@ func (v Vmap) Length() int {
 	return len(v.Hosts) + len(v.Guests)
 }
 
-// ParseVirsh parses the output of an Ansible run of
+// Load Loads the Ansible output file and parses it into the Vmap
+func (v *Vmap) Load(ansibleOutputFilename string) error {
+	raw, err := ioutil.ReadFile(ansibleOutputFilename)
+	if err != nil {
+		return err
+	}
+	x := ParseAnsibleOutput(raw)
+	v.Hosts, v.Guests = x.Hosts, x.Guests
+	return nil
+}
+
+// Get returns a host from the map.  The target host
+// may be a virtual host or a virtual guest.
+// nodeNotFoundErr is returned when the target is not in the map
+func (v Vmap) Get(target string) (*Vmap, error) {
+	for n, h := range v.Hosts {
+		if n == target {
+			return &Vmap{Hosts: map[string]VHost{n: h}}, nil
+		}
+	}
+	for n, g := range v.Guests {
+		if n == target {
+			return &Vmap{Guests: map[string]VGuest{n: g}}, nil
+		}
+	}
+	return nil, ErrNodeNotFound
+}
+
+// Info returns a friendly text string describing the target host.
+// Used in user cli queries.
+func (v *Vmap) Info(target string) string {
+	result, err := v.Get(target)
+	if err != nil {
+		return fmt.Sprintf("Node %s not found", target)
+	}
+	var info string
+	if h, ok := result.Hosts[target]; ok {
+		info = fmt.Sprintf("%s is a virtual host for guests: %s", target, strings.Join(h.Guests, ", "))
+	}
+	if g, ok := result.Guests[target]; ok {
+		info = fmt.Sprintf("%s is a virtual guest on host: %s", target, g.Host)
+	}
+	return info
+}
+
+// SafeVmap is a Vmap wrapped with a mutex for the
+// server to use since Go maps are not thread safe
+type SafeVmap struct {
+	sync.RWMutex
+	Vmap
+}
+
+// Length for SafeVmap wraps Vmap.Length() in a read lock
+// s is a pointer receiver so we don't copy the mutex
+func (s *SafeVmap) Length() int {
+	s.RLock()
+	defer s.RUnlock()
+	return s.Vmap.Length()
+}
+
+// Load for SafeVmap wraps Vmap.Load() in a (write) lock
+func (s *SafeVmap) Load(ansibleOutputFilename string) error {
+	s.Lock()
+	defer s.Unlock()
+	return s.Vmap.Load(ansibleOutputFilename)
+}
+
+// Get for SafeVmap wraps Vmap.Get() in a read lock
+// s is a pointer receiver so we don't copy the mutex
+func (s *SafeVmap) Get(target string) (*Vmap, error) {
+	s.RLock()
+	defer s.RUnlock()
+	return s.Vmap.Get(target)
+}
+
+// Info for SafeVmap wraps Vmap.Info() in a read lock
+// s is a pointer receiver so we don't copy the mutex
+func (s *SafeVmap) Info(target string) string {
+	s.RLock()
+	defer s.RUnlock()
+	return s.Vmap.Info(target)
+}
+
+// ParseAnsibleOutput parses the output of an Ansible run of
 // "virsh list --all" on all the virtual hosts
-func (v *Vmap) ParseVirsh(virshOutput []byte) {
-	v.Hosts = make(map[string]VHost)
-	v.Guests = make(map[string]VGuest)
+func ParseAnsibleOutput(ansibleOutput []byte) *Vmap {
+	v := &Vmap{
+		Hosts:  make(map[string]VHost),
+		Guests: make(map[string]VGuest),
+	}
 	nodename := ""
-	for _, line := range strings.Split(string(virshOutput), "\n") {
+	for _, line := range strings.Split(string(ansibleOutput), "\n") {
 		// Ansible status lines contain the hostname and any connection errors
 		if strings.Contains(line, " | ") {
 			nodename = strings.Split(strings.Fields(line)[0], ".")[0]
@@ -74,59 +157,5 @@ func (v *Vmap) ParseVirsh(virshOutput []byte) {
 	for _, h := range v.Hosts {
 		sort.Strings(h.Guests)
 	}
-}
-
-// Load Loads the Ansible output file and parses it into the Vmap
-func (v *Vmap) Load(virshFilename string) error {
-	raw, err := ioutil.ReadFile(virshFilename)
-	if err != nil {
-		return err
-	}
-	v.ParseVirsh(raw)
-	return nil
-}
-
-// Get returns a host from the map.  The target host
-// may be a virtual host or a virtual guest.
-// nodeNotFoundErr is returned when the target is not in the map
-func (v Vmap) Get(target string) (Vmap, error) {
-	var result Vmap
-	found := false
-	for n, h := range v.Hosts {
-		if n == target {
-			result.Hosts = make(map[string]VHost)
-			result.Hosts[n] = h
-			found = true
-			break
-		}
-	}
-	for n, g := range v.Guests {
-		if n == target {
-			result.Guests = make(map[string]VGuest)
-			result.Guests[n] = g
-			found = true
-			break
-		}
-	}
-	if !found {
-		return result, errNodeNotFound
-	}
-	return result, nil
-}
-
-// Info returns a friendly text string describing the target host.
-// Used in user cli queries.
-func (v Vmap) Info(target string) string {
-	result, err := v.Get(target)
-	if err != nil {
-		return fmt.Sprintf("Node %s not found", target)
-	}
-	var info string
-	if h, isHost := result.Hosts[target]; isHost {
-		info = fmt.Sprintf("%s is a virtual host for guests: %s", target, strings.Join(h.Guests, ", "))
-	}
-	if g, isGuest := result.Guests[target]; isGuest {
-		info = fmt.Sprintf("%s is a virtual guest on host: %s", target, g.Host)
-	}
-	return info
+	return v
 }
--- virtmapper.go
+++ virtmapper.go
@@ -2,99 +2,27 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
-
-	"github.com/urfave/cli"
 )
 
-// version constants
+// Version constants
 const (
-	version    = "0.0.3"
-	apiVersion = "v1"
+	Version    = "0.0.5"
+	APIVersion = "v1"
 )
 
 // Configuration defaults
 const (
-	listenAddress     = ":7474"
-	logFile           = "/var/log/virtmapper"
-	refreshInterval   = 60 // Minutes
-	ansibleOutputFile = "/tmp/virsh.txt"
+	ListenAddress     = ":7474"
+	LogFile           = "/var/log/virtmapper"
+	RefreshInterval   = 60 // Minutes
+	AnsibleOutputFile = "/tmp/virtmapper.txt"
 )
 
 func main() {
-	app := cli.NewApp()
-	app.Name = "virtmapper"
-	app.Usage = "maps libvirt virtual guests to their hosts"
-	app.Authors = []cli.Author{cli.Author{
-		Name:  "Derek Crovo",
-		Email: "dcrovo@gmail.com",
-	}}
-	app.Version = version
-	cli.AppHelpTemplate = VirtmapperHelpTemplate
-	app.Action = func(c *cli.Context) {
-		cli.ShowAppHelp(c)
+	app := CLIApp()
+	err := app.Run(os.Args)
+	if err != nil {
+		fmt.Printf("App error: %v\n", err)
 	}
-
-	app.Commands = []cli.Command{
-		{
-			Name:    "serve",
-			Aliases: []string{"s"},
-			Usage:   "run the server and accept map queries",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "address, a",
-					Value: listenAddress,
-					Usage: "address and port to listen on",
-				},
-				cli.StringFlag{
-					Name:  "logfile, l",
-					Value: logFile,
-					Usage: "log file for server activity",
-				},
-				cli.IntFlag{
-					Name:  "refreshInterval, r",
-					Value: refreshInterval,
-					Usage: "map refresh interval in minutes",
-				},
-				cli.StringFlag{
-					Name:  "ansibleOutputFile, v",
-					Value: ansibleOutputFile,
-					Usage: "path to Ansible output file to read",
-				},
-			},
-			Action: func(c *cli.Context) {
-				f, err := os.OpenFile(c.String("logfile"), os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-				if err != nil {
-					fmt.Printf("Error opening file: %v\n", err)
-					os.Exit(1)
-				}
-				defer f.Close()
-				log.SetOutput(f)
-				v := server{Reloader(c.String("ansibleOutputFile"), c.Int("refreshInterval"))}
-				v.Serve(c.String("address"))
-			},
-		},
-		{
-			Name:    "query",
-			Aliases: []string{"q"},
-			Usage:   "query a server with the given request",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name: "server, s",
-					// Value: "manager.corp.airwave.com:7474",
-					Usage: "address of server to query",
-				},
-			},
-			Action: func(c *cli.Context) {
-				result, err := Query(c.String("server"), c.Args().Get(0))
-				if err != nil {
-					os.Exit(1)
-				}
-				Display(result)
-			},
-		},
-	}
-
-	app.Run(os.Args)
 }
